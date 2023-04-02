c584f297ee4c23022b19a64eb843f1991a74f416
BlackArcherr <70406811+BlackArcherr@users.noreply.github.com>
2023-04-02 03:15:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import t
 from scipy.stats import norm
 
 
 chat_id = 474140315 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    a = 2 * x / t**2
+    a = 2 * x / 92**2
     x_bar = np.mean(a)
     s = np.std(a, ddof=1)
-    se = s / np.sqrt(n)
-    df = n - 1
+    se = s / np.sqrt(len(x))
+    df = len(x) - 1
     t_value = t.ppf(1 - (1 - p) / 2, df)
     me = t_value * se
     ci = (x_bar - me, x_bar + me)
